69c465ab035b24984abd9a9ea891b6157f8fc4de
rizkijuliansyaham <rizki.juliansyah.a.m@gmail.com>
2022-07-31 16:40:00 +0700

    fix tanggal pick

--- lib/pages/transaksi_page.dart
+++ lib/pages/transaksi_page.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:test_qtasnim/models/transaksi_model.dart';
 import 'package:test_qtasnim/services/transaksi_repo.dart';
 import 'package:test_qtasnim/utils/theme.dart';
 import 'package:http/http.dart' as http;
@@ -17,18 +19,38 @@ class TransaksiPage extends StatefulWidget {
 
 class _TransaksiPageState extends State<TransaksiPage> {
   // List dataTransaksi = [];
+  DateTime currentDate = DateTime.now();
+  final _formKey = GlobalKey<FormState>();
   late StreamController dataTransaksi;
+  late StreamController dataBarang;
   bool isDataLoading = false;
+  int idTerakhir = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     // fetchDataTransaksi();
+    DateTime currentDate = DateTime.now();
     dataTransaksi = new StreamController();
+    dataBarang = new StreamController();
     Timer.periodic(Duration(milliseconds: 300), (_) {
       loadDataTransaksi();
+      loadDataBarang();
     });
     super.initState();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2050));
+    if (pickedDate != null && pickedDate != currentDate)
+      setState(() {
+        currentDate = pickedDate;
+        print(currentDate);
+      });
   }
 
   Future fetchDataTransaksi() async {
@@ -54,11 +76,200 @@ class _TransaksiPageState extends State<TransaksiPage> {
     // dataTransaksi.addAll(TransaksiRepo.transaksiModel!.);
   }
 
+  Future fetchDataBarang() async {
+    try {
+      isDataLoading = true;
+      final response =
+          await http.get(Uri.tryParse('http://192.168.8.101:5000/Barang')!);
+      if (response.statusCode == 200) {
+        // print(response.body);
+        return json.decode(response.body);
+
+        // final response = await TransaksiRepo.getDataTransaksi();
+        // print(response);
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      isDataLoading = false;
+    }
+    // await TransaksiRepo.getDataTransaksi();
+    // dataTransaksi.addAll(TransaksiRepo.transaksiModel!.);
+  }
+
   loadDataTransaksi() async {
     fetchDataTransaksi().then((res) async {
       dataTransaksi.add(res);
       return res;
     });
+  }
+
+  loadDataBarang() async {
+    fetchDataBarang().then((res) async {
+      dataBarang.add(res);
+      return res;
+    });
+  }
+
+  void deleteDataTransaksi(int id) async {
+    var delete = await TransaksiRepo.deleteTransaksi(id);
+  }
+
+  Future<void> showDialogDelete(
+      BuildContext context, String pesan, int id) async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(pesan,
+                    style: TextStyle(
+                        color: BaseTheme.color, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            actions: [
+              InkWell(
+                onTap: (() {
+                  Navigator.of(context).pop();
+                }),
+                child: Container(
+                  height: 40,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(color: BaseTheme.color, width: 0.4)),
+                  child: Center(
+                    child: Text(
+                      "Tidak",
+                      style: TextStyle(
+                          color: BaseTheme.color, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (() {
+                  deleteDataTransaksi(id);
+                  Navigator.of(context).pop();
+                }),
+                child: Container(
+                  height: 40,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: BaseTheme.color,
+                      borderRadius: BorderRadius.circular(4)),
+                  child: Center(
+                    child: Text(
+                      "Ya",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> showDialogAdd(BuildContext context, int id) async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          final TextEditingController namaBarang = TextEditingController();
+          final TextEditingController jumlahTerjual = TextEditingController();
+
+          return AlertDialog(
+            content: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Tambah Transaksi",
+                        style: TextStyle(
+                            color: BaseTheme.color,
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 12),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: namaBarang,
+                      decoration: InputDecoration(
+                          hintText: 'Contoh : Teh', labelText: 'Nama Barang'),
+                      validator: (value) {
+                        if (value!.isNotEmpty) {
+                          return null;
+                        } else {
+                          return "Harus diisi";
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: jumlahTerjual,
+                      decoration: InputDecoration(
+                          hintText: 'Contoh : 12', labelText: 'Jumlah Terjual'),
+                      validator: (value) {
+                        if (value!.isNotEmpty) {
+                          return null;
+                        } else {
+                          return "Harus diisi";
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          _selectDate(context);
+                        },
+                        icon: Icon(Icons.date_range_outlined))
+                  ],
+                )),
+            actions: [
+              InkWell(
+                onTap: (() {
+                  if (_formKey.currentState!.validate()) {
+                    // putStatusKontrolAuto(1, index);
+                    int id_transaksi = id + 1;
+
+                    String nama_barang = namaBarang.text.toString();
+                    int jumlah_terjual =
+                        int.parse(jumlahTerjual.text.toString());
+                    String tanggal = currentDate.toString();
+                    List<String> tanggalSplit = tanggal.split(' ');
+                    String tanggal_transaksi = '${tanggalSplit[0]}';
+                    print(tanggal_transaksi);
+                    // putStatusKontrolParameter("$parameter", index);
+                    TransaksiRepo.addTransaksi(
+                        id, nama_barang, jumlah_terjual, tanggal_transaksi);
+
+                    Navigator.of(context).pop();
+                  }
+                }),
+                child: Container(
+                  height: 40,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: BaseTheme.color,
+                      borderRadius: BorderRadius.circular(4)),
+                  child: Center(
+                    child: Text(
+                      "Tambah",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -69,7 +280,7 @@ class _TransaksiPageState extends State<TransaksiPage> {
           physics: ClampingScrollPhysics(),
           children: [
             SizedBox(
-              height: 105,
+              height: 110,
             ),
             StreamBuilder<dynamic>(
                 stream: dataTransaksi.stream,
@@ -79,7 +290,8 @@ class _TransaksiPageState extends State<TransaksiPage> {
                   } else if (snapshot.hasData) {
                     var jumlahData = snapshot.data!.length;
                     var data = snapshot.data;
-                    print(jumlahData);
+                    // print(jumlahData);
+
                     return Container(
                       width: double.infinity,
                       height: MediaQuery.of(context).size.height,
@@ -92,49 +304,64 @@ class _TransaksiPageState extends State<TransaksiPage> {
                           var tanggal = arr[0].split('-');
                           var tanggalfix =
                               '${tanggal[2]}-${tanggal[1]}-${tanggal[0]}';
+
+                          var idBelakang = data[jumlahData - 1]['id_transaksi'];
+                          idTerakhir = idBelakang;
+
+                          // print(idBelakang);
                           return Padding(
                             padding: const EdgeInsets.only(
                                 left: 8.0, right: 8.0, top: 4, bottom: 4),
                             child: Container(
                               height: 50,
                               width: double.infinity,
-                              color: Colors.grey,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(data[index]['nama_barang']),
-                                  Text(
-                                      data[index]['jumlah_terjual'].toString()),
-                                  Text(tanggalfix),
-                                ],
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Expanded(
+                                        flex: 1,
+                                        child:
+                                            Text(data[index]['nama_barang'])),
+                                    Expanded(
+                                      flex: 1,
+                                      child: Text(data[index]['jumlah_terjual']
+                                          .toString()),
+                                    ),
+                                    Expanded(flex: 1, child: Text(tanggalfix)),
+                                    IconButton(
+                                        onPressed: () {
+                                          // deleteDataTransaksi(
+                                          //     data[index]['id_transaksi']);
+                                          showDialogDelete(
+                                              context,
+                                              "Apakah Anda yakin ingin menghapus data ini?",
+                                              data[index]['id_transaksi']);
+                                        },
+                                        icon: Icon(
+                                          Icons.delete,
+                                          size: 18,
+                                          color: Colors.red,
+                                        ))
+                                  ],
+                                ),
                               ),
                             ),
                           );
                         },
                       ),
                     );
-                    // return ListView.builder(
-                    //   itemBuilder: (context, index) {
-                    //     return Padding(
-                    //       padding: const EdgeInsets.only(
-                    //           left: 8.0, right: 8.0, top: 4, bottom: 4),
-                    //       child: Container(
-                    //         height: 50,
-                    //         width: double.infinity,
-                    //         color: Colors.grey,
-                    //       ),
-                    //     );
-                    //   },
-                    // );
                   } else if (snapshot.connectionState != ConnectionState.done) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8.0, right: 8.0, top: 4, bottom: 4),
-                      child: Container(
-                        height: 50,
-                        width: double.infinity,
-                        color: Colors.grey,
+                    return Container(
+                      height: 200,
+                      child: Center(
+                        child: CircularProgressIndicator(),
                       ),
                     );
                   } else if (!snapshot.hasData &&
@@ -145,15 +372,6 @@ class _TransaksiPageState extends State<TransaksiPage> {
                     return Expanded(
                         child: Center(child: Text('Tidak Ada Data')));
                   }
-                  // return Padding(
-                  //   padding: const EdgeInsets.only(
-                  //       left: 8.0, right: 8.0, top: 4, bottom: 4),
-                  //   child: Container(
-                  //     height: 50,
-                  //     width: double.infinity,
-                  //     color: Colors.grey,
-                  //   ),
-                  // );
                 }),
             SizedBox(
               height: 70,
@@ -170,17 +388,38 @@ class _TransaksiPageState extends State<TransaksiPage> {
                 color: Colors.red,
               ),
               Container(
-                height: 30,
+                height: 40,
                 width: double.infinity,
                 color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.only(right: 40),
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text("Nama Barang"),
-                      Text("Stok Keluar"),
-                      Text("Tanggal"),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          "Nama\nBarang",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          "Jumlah\nTerjual",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Tanggal",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        "act",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
                 ),
@@ -195,19 +434,24 @@ class _TransaksiPageState extends State<TransaksiPage> {
             width: double.infinity,
             // color: Colors.red,
             child: Center(
-                child: Container(
-              height: 40,
-              width: 140,
-              // color: Colors.green,
-              decoration: BoxDecoration(
-                  color: BaseTheme.color,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Center(
-                  child: Text(
-                "Tambah Transaksi",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-              )),
+                child: InkWell(
+              onTap: () {
+                showDialogAdd(context, idTerakhir + 1);
+              },
+              child: Container(
+                height: 40,
+                width: 140,
+                // color: Colors.green,
+                decoration: BoxDecoration(
+                    color: BaseTheme.color,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Text(
+                  "Tambah Transaksi",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                )),
+              ),
             )),
           ),
         )
